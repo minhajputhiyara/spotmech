b659f03013b0806c792f652dcb7eb9b4b93a5ee2
Minhaj <minhajputhiyara@gmail.com>
2023-06-20 00:06:19 +0530

    spotmech project completed successfully

---
+++ Spotmech/vehicleservicemanagement-master/vehicle/forms.py
@@ -0,0 +1,113 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+from django.core.validators import MaxValueValidator
+from .models import Mechanic, Location
+
+class CustomerUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model=models.Customer
+        fields=['address','mobile','profile_pic']
+
+
+class MechanicUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+
+
+
+class MechanicForm(forms.ModelForm):
+    latitude = forms.DecimalField(max_digits=9, decimal_places=6, required=False)
+    longitude = forms.DecimalField(max_digits=9, decimal_places=6, required=False)
+
+    class Meta:
+        model = Mechanic
+        fields = ['address', 'mobile', 'profile_pic', 'skill']
+
+    def save(self, commit=True):
+        mechanic = super(MechanicForm, self).save(commit=False)
+        if commit:
+            mechanic.save()
+
+        latitude = self.cleaned_data.get('latitude')
+        longitude = self.cleaned_data.get('longitude')
+
+        if latitude and longitude:
+            location = Location.objects.create(latitude=latitude, longitude=longitude)
+            mechanic.location = location
+            mechanic.save()
+
+        return mechanic
+
+
+class MechanicRatingForm(forms.Form):
+    rating=forms.DecimalField(
+        decimal_places=2,
+        max_digits=5,
+        validators=[MaxValueValidator(10)]
+    )
+
+
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model=models.Request
+        fields=['category','vehicle_no','vehicle_name','vehicle_model','vehicle_brand','problem_description']
+        widgets = {
+        'problem_description':forms.Textarea(attrs={'rows': 3, 'cols': 30})
+        }
+
+class AdminRequestForm(forms.Form):
+    #to_field_name value will be stored when form is submitted.....__str__ method of customer model will be shown there in html
+    customer=forms.ModelChoiceField(queryset=models.Customer.objects.all(),empty_label="Customer Name",to_field_name='id')
+    mechanic=forms.ModelChoiceField(queryset=models.Mechanic.objects.all(),empty_label="Mechanic Name",to_field_name='id')
+    cost=forms.IntegerField()
+
+class AdminApproveRequestForm(forms.Form):
+    mechanic=forms.ModelChoiceField(queryset=models.Mechanic.objects.all(),empty_label="Mechanic Name",to_field_name='id')
+    cost=forms.IntegerField()
+    stat=(('Pending','Pending'),('Approved','Approved'),('Released','Released'))
+    status=forms.ChoiceField( choices=stat)
+
+
+class UpdateCostForm(forms.Form):
+    cost=forms.IntegerField()
+
+class MechanicUpdateStatusForm(forms.Form):
+    stat=(('Approved','Approved'),('Repairing','Repairing'),('Repairing Done','Repairing Done'))
+    status=forms.ChoiceField( choices=stat)
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model=models.Feedback
+        fields=['by','message']
+        widgets = {
+        'message':forms.Textarea(attrs={'rows': 6, 'cols': 30})
+        }
+
+#for Attendance related form
+presence_choices=(('Present','Present'),('Absent','Absent'))
+class AttendanceForm(forms.Form):
+    present_status=forms.ChoiceField( choices=presence_choices)
+    date=forms.DateField()
+
+class AskDateForm(forms.Form):
+    date=forms.DateField()
+
+
+#for contact us page
+class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
---
+++ Spotmech/vehicleservicemanagement-master/vehicle/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/CustomerProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
+    def __str__(self):
+        return self.user.first_name
+
+
+
+class Location(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+class Mechanic(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pic/MechanicProfilePic/', null=True, blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20, null=False)
+    skill = models.CharField(max_length=500, null=True)
+    rating = models.DecimalField(decimal_places=2, max_digits=5, null=True)
+    status = models.BooleanField(default=False)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
+
+    @property
+    def get_name(self):
+        return self.user.first_name + " " + self.user.last_name
+
+    @property
+    def get_id(self):
+        return self.user.id
+
+    def __str__(self):
+        return self.user.first_name
+
+
+
+class Request(models.Model):
+    cat=(('two wheeler with gear','two wheeler with gear'),('two wheeler without gear','two wheeler without gear'),('three wheeler','three wheeler'),('four wheeler','four wheeler'))
+    category=models.CharField(max_length=50,choices=cat)
+
+    vehicle_no = models.CharField(max_length=40,null=False)
+    vehicle_name = models.CharField(max_length=40,null=False)
+    vehicle_model = models.CharField(max_length=40,null=False)
+    vehicle_brand = models.CharField(max_length=40,null=False)
+
+    problem_description = models.CharField(max_length=500,null=False)
+    date=models.DateField(auto_now=True)
+    cost=models.PositiveIntegerField(null=True)
+
+    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    mechanic=models.ForeignKey('Mechanic',on_delete=models.CASCADE,null=True)
+
+    stat=(('Pending','Pending'),('Approved','Approved'),('Repairing','Repairing'),('Repairing Done','Repairing Done'),('Released','Released'))
+    status=models.CharField(max_length=50,choices=stat,default='Pending',null=True)
+
+    def __str__(self):
+        return self.problem_description
+
+class Attendance(models.Model):
+    mechanic=models.ForeignKey('Mechanic',on_delete=models.CASCADE,null=True)
+    date=models.DateField()
+    present_status = models.CharField(max_length=10)
+
+class Feedback(models.Model):
+    date=models.DateField(auto_now=True)
+    by=models.CharField(max_length=40)
+    message=models.CharField(max_length=500)
+
